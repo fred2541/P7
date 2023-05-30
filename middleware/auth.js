def34d835830e2d8e80766dabe6b104e9026394e
fred2541 <fredionze@free.fr>
2023-05-30 09:11:41 +0200

    Secure Auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,8 +5,10 @@ const RANDOM_TOKEN_SECRET = process.env.RANDOM_TOKEN_SECRET;
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
+        !token ? res.status(401).json({ error}) : null; // Check Token present
         const decodedToken = jwt.verify(token, RANDOM_TOKEN_SECRET);
         const userId = decodedToken.userId;
+        !userId ? res.status(401).json({ error }) : null; // Verify Token validyti
         req.auth = {
             userId: userId
         };
