6a4a7b99d08ff40ff221ab3e3091fe0c5b8b2af7
fred2541 <fredionze@free.fr>
2023-05-30 10:27:57 +0200

    Add regex for checkInput

--- middleware/check_Inputs_Add_Book.js
+++ middleware/check_Inputs_Add_Book.js
@@ -2,14 +2,21 @@ function genererErreur(message) {
     throw new Error(message);
   }
 
-module.exports = (req, res, next) => {
-    const bookObject = JSON.parse(req.body.book);
+ const titleRegex = /^[a-zA-Z0-9\s!?_]{1,80}$/; // a-z A-Z 0-9 /s(espace)  !?-_ (special charactere)
+const authorRegex = /^[a-zA-Z\s'-]{1,80}$/;
+const genreRegex = /^[a-zA-Z\s]{1,80}$/;
+const ratingRegex = /^[1-5]$/; // only 1-5
 
+module.exports = (req, res, next) => {
     try {
+        const bookObject = JSON.parse(req.body.book);
+        // Check if input valide format
         (Number(bookObject.year) > new Date(Date.now()).getFullYear()) || (Number(bookObject.year) < 0) ? genererErreur("Année de publication") : null ;
-        (bookObject.title.length > 80) || (bookObject.title.length <= 0) ? genererErreur("Titre") : null ;
-        (bookObject.author.length > 80) || (bookObject.author.length <= 0) ? genererErreur("Auteur") : null ;
-        (bookObject.genre.length > 80) || (bookObject.genre.length <= 0) ? genererErreur("Genre") : null ;        
+        !bookObject.title.match(titleRegex) ? genererErreur("Titre") : null ;
+        !bookObject.author.match(authorRegex) ? genererErreur("Auteur") : null ;
+        !bookObject.genre.match(genreRegex) ? genererErreur("Genre") : null ;
+        !bookObject.ratings[0].grade.toString().match(ratingRegex) ? genererErreur("Vote") : null ;
+        !bookObject.averageRating.toString().match(ratingRegex) ? genererErreur("Vote") : null ;
         next();
     } catch(error) {
         res.status(400).json({ message: error.toString() + ' invalide!' });
--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -5,9 +5,9 @@ const storage = multer.memoryStorage(); // No support for filename in memoryStor
 const fileFilter = (req, file, cb) => {
   // Check file type
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
-      cb(null, true); 
+      cb(null, true);
   } else {
-      cb(null, false); // Gen fileFilter error
+      cb(new Error('Type de fichier incorrecte !'), false); // Gen error for fileFilter
   }
 };
 
@@ -17,7 +17,7 @@ const upload = multer({ storage: storage, fileFilter: fileFilter }).single('imag
 const multerMiddleware = (req, res, next) => {
     upload(req, res, (error) => {
       // if error undefined => ok no error, else 400 => stop the route
-      error === undefined ? next() : res.status(400).json({ message: error });
+      error === undefined ? next() : res.status(400).json({ message: error.toString() });
     });
   };
 
